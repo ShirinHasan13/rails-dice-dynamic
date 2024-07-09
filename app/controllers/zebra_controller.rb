a56db364f43725a1ddb47c30edfaacf520f71eb9
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-09 00:02:53 +0000

    r

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -17,4 +17,43 @@ def giraffe
   
   render({ :template => "game_templates/two_six" })
 end
+
+def elephant
+  
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
+
+render({ :template => "game_templates/two_ten" })
+end
+
+def lion
+  
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
+render({ :template => "game_templates/one_twenty" })
+end
+
+def tiger
+  
+  @rolls = []
+
+  5.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
+
+render({ :template => "game_templates/five_four" })
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ get("/", { :controller => "zebra", :action => "rules" })
 
 get("/dice/2/6", { :controller => "zebra", :action => "giraffe" })
 
-get("/dice/2/10", { :controller => "zebra", :action => "giraffe" })
+get("/dice/2/10", { :controller => "zebra", :action => "elephant" })
 
-get("/dice/1/20", { :controller => "zebra", :action => "giraffe" })
+get("/dice/1/20", { :controller => "zebra", :action => "lion" })
 
-get("/dice/5/4", { :controller => "zebra", :action => "giraffe" })
+get("/dice/5/4", { :controller => "zebra", :action => "tiger" })
 end

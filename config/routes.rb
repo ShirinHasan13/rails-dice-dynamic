21f15dd81ba775cbd92656145bdfd008b28d373f
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-08-04 21:25:25 +0000

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-
-get("/", { :controller => "zebra", :action => "rules" })
-
-get("/dice/2/6", { :controller => "zebra", :action => "giraffe" })
-
-get("/dice/2/10", { :controller => "zebra", :action => "elephant" })
-
-get("/dice/1/20", { :controller => "zebra", :action => "lion" })
-
-get("/dice/5/4", { :controller => "zebra", :action => "tiger" })
+  get("/", { :controller => "dice", :action => "home" })
+  get("/dice/2/6", { :controller => "dice", :action => "two_six" })
+  get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
+  get("/dice/1/20", { :controller => "dice", :action => "one_twenty" })
+  get("/dice/5/4", { :controller => "dice", :action => "five_four" })
+  get("dice/:dice/:sides", { :controller => "dice", :action => "random" })
 end
